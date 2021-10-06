d8c17a64349254c25e70e7c914e4dbe73de28291
scaraclette <gustiscarlett@gmail.com>
2021-10-06 10:10:25 -0700

    added TODOs

--- webhooks-server/src/index.ts
+++ webhooks-server/src/index.ts
@@ -1,3 +1,31 @@
 import express from "express";
 const app = express();
 const port = 8080;
+
+app.use(express.json());
+
+// TODO: Potential save subscription name and ids to map
+
+app.get('/', (req, res) => {
+    // TODO: go to different endpoints
+});
+
+// TODO: endpoint to subscribe to firefly
+// Takes in name and designates it to URL 
+app.post('/subscribe-firefly', (req, res) => {
+    // TODO: accept subscription name to subscribe
+});
+
+// TODO: endpoint to unsubscribe to firefly
+app.post('/unsubscribe-firefly', (req, res) => {
+    // Accept subscription name
+});
+
+// TODO: endpoint to register webhook URI
+app.post('/webhook-firefly', (req, res) => {
+
+});
+
+app.listen(port, () => {
+
+});
